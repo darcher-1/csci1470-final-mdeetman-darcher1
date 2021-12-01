682d6c5a58395e9531dbacc9734bef437d357597
Mathijs Deetman <mdeetman@cs.brown.edu>
2021-12-01 16:48:34 -0500

    Add DQN network structure

---
+++ DQN.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+# Killing optional CPU driver warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+class DQNAgent(tf.keras.Model):
+    def __init__(self, num_actions):
+        super(DQNAgent, self).__init__()
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        self.network = tf.keras.Sequential()
+        self.network.add(tf.keras.layers.Conv2D(16, 8, strides=(4, 4), activation='relu'))
+        self.network.add(tf.keras.layers.Conv2D(32, 4, strides=(2, 2), activation='relu'))
+        self.network.add(tf.keras.layers.Flatten())
+        self.network.add(tf.keras.layers.Dense(256, activation='relu'))
+        self.network.add(tf.keras.layers.Dense(num_actions))
+
+    def call(self, state):
+        return self.network(state)
+
+
+
